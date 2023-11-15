d3b40b370db05efb1734554f907bc8f7101da147
CDE90 <ethan@ethancoward.dev>
2023-11-15 19:15:58 +0000

    Update deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env.mjs";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "mysql2",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["litics_*"],
+    schema: "./src/server/db/schema.ts",
+    driver: "mysql2",
+    dbCredentials: {
+        uri: env.DATABASE_URL,
+    },
+    tablesFilter: ["litics_*"],
 } satisfies Config;
