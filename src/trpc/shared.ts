b0b1deb9ec71804d15190fdc6e5c45b2a65f09eb
Ethan Coward <ethan@ethancoward.dev>
2023-12-07 11:08:25 +0000

    Fix getBaseUrl

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -1,5 +1,6 @@
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
+import { env } from "~/env.mjs";
 
 import { type AppRouter } from "~/server/api/root";
 
@@ -8,7 +9,7 @@ export const transformer = superjson;
 export function getBaseUrl() {
     if (typeof window !== "undefined") return "";
     const vc = process.env.VERCEL_URL;
-    if (vc) return "https://" + vc;
+    if (vc) return "https://" + env.NEXTAUTH_URL;
     return "http://localhost:3000";
 }
 
